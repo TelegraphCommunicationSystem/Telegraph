8bf986b542534958cf0020dc031bac561e5e5285
Andre Bogner <andre@andrebogner.de>
2025-06-10 21:51:25 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import wifi_manager
+
+wlan = wifi_manager.get_connection()
+if wlan is None:
+    print("Could not initialize the network connection.")
+    while True:
+        pass  # you shall not pass :D
+
